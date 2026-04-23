bd98ca04f99b5db6c666d11e129d27d53bbdc7a8
vanh <87958434+vanh215@users.noreply.github.com>
2026-04-23 12:32:05 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+# Lỗi bảo mật chí mạng: Hardcode Secret Key thẳng vào mã nguồn!
+SECRET_API_KEY = "AKIAIOSFODNN7EXAMPLE" 
+
+def login():
+    print("Đang kết nối hệ thống với key:", SECRET_API_KEY)
+
+if __name__ == "__main__":
+    login()
